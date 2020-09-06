59d3458ee667b408dc62a01596e0274ef0d06175
SalahEddine <ab.salah012@gmail.com>
2020-09-06 23:33:08 +0100

    Updates

---
+++ precache-manifest.408733d56ba35828f8244ba823c50a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ba35dc4cec56e665f5dbd3bc235ec9",
+    "url": "/Covid-19-Statistics/index.html"
+  },
+  {
+    "revision": "0de0a4458eb525a8348e",
+    "url": "/Covid-19-Statistics/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d15dd65ed4f74be35bbf",
+    "url": "/Covid-19-Statistics/static/css/main.ba366ab2.chunk.css"
+  },
+  {
+    "revision": "0de0a4458eb525a8348e",
+    "url": "/Covid-19-Statistics/static/js/2.087ef88b.chunk.js"
+  },
+  {
+    "revision": "e7043f0f3d85f5377ce15025b39a9408",
+    "url": "/Covid-19-Statistics/static/js/2.087ef88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15dd65ed4f74be35bbf",
+    "url": "/Covid-19-Statistics/static/js/main.08adc361.chunk.js"
+  },
+  {
+    "revision": "8c4af6e3ed15750cc418",
+    "url": "/Covid-19-Statistics/static/js/runtime-main.4a53d11e.js"
+  }
+]);
